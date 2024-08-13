9c315aacf260e3e5202cd3112d1f1e7c036d7563
KewirJewel <kewirlinx@gmail.com>
2024-08-12 22:23:46 -0700

    Display Hello, World!

---
+++ Day1/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+ <style>
+ 
+    body {
+                   font-family: Arial, sans-serif;
+                   text-align: center;
+                   background-color: gray;
+               }
+    form {
+               text-align: center;
+                   color: blue;
+                   display: flexbox;
+                   font-size: xxx-large;
+                   width: 100%;
+                  
+    }
+    
+               </style>
+
+</head>
+<body>
+   <form action="index.php">
+   <?php 
+  $world = "Hello, World!";
+   echo $world
+   ?>
+   </form>
+   
+
+</body>
+</html> 
+ 
+ 
